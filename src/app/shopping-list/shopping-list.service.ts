ee49bb62bf1315a2f0b3101026b3ae69bfa89410
Hanzhang ma <583886027@qq.com>
2023-04-16 11:46:54 +0200

    120-121 adding shoppinglist Service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,18 @@
-export class ShoppingListService{
+import {Ingredient} from "../shared/ingredient.model";
+import {EventEmitter, Injectable, Output} from "@angular/core";
 
+@Injectable()
+export class ShoppingListService{
+  @Output() ingredientChanges = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
+  public getIngredients(){
+    return this. ingredients.slice();
+  }
+  public addIngredients(ingredient){
+    this.ingredients.push(ingredient);
+    this.ingredientChanges.emit(this.ingredients.slice());
+  }
 }
