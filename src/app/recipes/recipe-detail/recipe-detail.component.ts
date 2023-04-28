fd4ab684d59ce4b498caa9cf1890e1cd98398603
Hanzhang ma <583886027@qq.com>
2023-04-28 17:34:30 +0200

    168 Add links of the button
    add a router:Router at the constructor
    
    use router.navigate(['position'],{relativeTo: this.route) to navigate the website to the right component.

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -3,7 +3,7 @@ import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import { Recipe } from '../recipe.model';
 import {Ingredient} from "../../shared/ingredient.model";
 import {RecipeService} from "../recipe.service";
-import {ActivatedRoute, Params} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-recipe-detail',
@@ -16,7 +16,7 @@ export class RecipeDetailComponent implements OnInit {
 
   id: number;
 
-  constructor(private recipeService: RecipeService, private route: ActivatedRoute) { }
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute, private router:Router) { }
 
   ngOnInit() {
     // const id = this.route.snapshot.params['id'];
@@ -31,6 +31,10 @@ export class RecipeDetailComponent implements OnInit {
   onClickToShoppingList(){
     console.log(this.recipe.ingredients);
     this.addToShoppingList.emit(this.recipe.ingredients);
+  }
+
+  onEditRecipe(){
+    this.router.navigate(['edit'],{relativeTo: this.route});
   }
 
 }
